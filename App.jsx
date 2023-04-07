381271a7aad5ef052233fbf11dee7adb8f9afc4c
Lupey1103 <luperon.christian95@gmail.com>
2023-04-06 22:30:17 -0400

    Setting up src folder

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./src/screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="GetInk'D" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
