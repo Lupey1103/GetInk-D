4577f9705c1a00b4437226febd0054e055289d0b
Lupey1103 <luperon.christian95@gmail.com>
2023-04-11 17:25:21 -0400

    Added Searchbar and some shops as dummy shops. Need to add more and functionality to everything

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Entypo, AntDesign, Ionicons, FontAwesome } from "@expo/vector-icons";
+import { Catagories } from "../components/Catagories";
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,13 +15,14 @@ export const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className="bg-orange-400 pt-2 rounded-md">
+    <View>
+      <SafeAreaView className="bg-orange-400 pt-2 rounded-md">
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
             uri: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
           }}
-          className="h-7 w-7 bg-gray-300 rounded-full"
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
 
         <View className="flex-1">
@@ -35,19 +37,23 @@ export const HomeScreen = () => {
 
         <AntDesign name="bars" size={35} color="#f8fafc" />
       </View>
-      <View>
-        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3 rounded-lg">
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3 rounded-xl'>
           <FontAwesome name="search" size={24} color="#707070" />
-          <TextInput placeholder="Parlors and Artists" keyboardType="default"/>
-          </View>
-          <Ionicons name="filter" size={24} color="#f8fafc" />
+          <TextInput
+            placeholder="Parlors and Artists"
+            keyboardType="default"
+          />
         </View>
-        <ScrollView>
-          
-        </ScrollView>
+        <Ionicons name="filter" size={24} color="#f8fafc" />
       </View>
+      
     </SafeAreaView>
+    <ScrollView>
+      <Catagories />
+    </ScrollView>
+    </View>
+    
   );
 };
 
